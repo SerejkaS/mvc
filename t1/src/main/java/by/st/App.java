0b742fc82321799235ebbfb07907001bd5bbf911
Andrew Reut <AndrewExample@gmail.com>
2016-01-18 20:20:56 +0300

    new rows

--- t1/src/main/java/by/st/App.java
+++ t1/src/main/java/by/st/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! 1111111" );
+        System.out.println( "Hello World! 1111111 2222" );
     }
 
     public void newMethod(){
