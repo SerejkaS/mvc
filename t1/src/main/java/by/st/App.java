acd7f881f9a25b8b37ef4569d8ce4cb5d9c217f5
Andrew Reut <AndrewExample@gmail.com>
2016-01-18 20:10:19 +0300

    my commit

--- t1/src/main/java/by/st/App.java
+++ t1/src/main/java/by/st/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World! 1111111" );
     }
+
+    public void newMethod(){
+        System.out.println("New method");
+    }
+
 }
