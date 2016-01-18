efd8069dc2c567fc806b5821fbcdbe0e61ee54d3
SerejkaS <syrskiy@gmail.com>
2016-01-18 20:26:59 +0300

    commit S 2

--- t1/src/main/java/by/st/App.java
+++ t1/src/main/java/by/st/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from SerejkaS" );
-        System.out.println( "Hello from SerejkaS  222" );
+        System.out.println("Hello from SerejkaS");
+        System.out.println("Hello from SerejkaS  222");
     }
 }
